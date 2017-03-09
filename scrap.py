1907a360cc7bd4a84d7a937d130bfa0098d237c2
Matthew Hassell <hassell.matthew@gmail.com>
2017-03-08 20:03:19 -0500

    some noodling

--- scrap.py
+++ scrap.py
@@ -31,6 +31,54 @@ def host():
 		ready_to_read,ready_to_write,in_error = select.select(SOCKET_LIST,[],[],0)
 
 		for sock in ready_to_read:
+			# we have a new connection request 
+			# (does the host only have something to read if someone connects?) (played with in 
+			# console - yes, host reports it has something to read if someone connects)
+			if sock == server_socket:
+				sockfd, addr = server_socket.accept()
+				SOCKET_LIST.append(sockfd)
+				print "Client (%s, %s) connected" % addr
 
-			# we have a new connection request
-			
+				# let everyone know someone signed in
+				broadcast(server_socket, sockfd, "[%s:%s] entered our chatting room\n" % addr)
+
+			# message from a client
+			else:
+                # process data recieved from client, 
+                try:
+                    # receiving data from the socket.
+                    data = sock.recv(RECV_BUFFER)
+                    if data:
+                        # there is something in the socket
+                        broadcast(server_socket, sock, "\r" + '[' + str(sock.getpeername()) + '] ' + data)  
+                    else:
+                        # remove the socket that's broken    
+                        if sock in SOCKET_LIST:
+                            SOCKET_LIST.remove(sock)
+
+                        # at this stage, no data means probably the connection has been broken
+                        broadcast(server_socket, sock, "Client (%s, %s) is offline\n" % addr) 
+
+                # exception 
+                except:
+                    broadcast(server_socket, sock, "Client (%s, %s) is offline\n" % addr)
+                    continue
+
+    server_socket.close()
+
+def broadcast(server_socket, sock, message):
+	for socket in SOCKET_LIST:
+        # send the message only to peer
+        if socket != server_socket and socket != sock :
+            try :
+                socket.send(message)
+            except :
+                # broken socket connection
+                socket.close()
+                # broken socket, remove it
+                if socket in SOCKET_LIST:
+                    SOCKET_LIST.remove(socket)
+ 
+if __name__ == "__main__":
+
+    sys.exit(chat_server())
