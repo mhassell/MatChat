119ad4e7ed221b6657dfd8a7bff9d01bd572396b
Matthew Hassell <hassell.matthew@gmail.com>
2017-11-01 18:36:23 -0400

    frustrated

--- boost-practice/Timer3.cpp
+++ boost-practice/Timer3.cpp
@@ -3,3 +3,30 @@
 #include <boost/bind.hpp>
 #include <boost/date_time/posix_time/posix_time.hpp>
 
+void print(const boost::system::error_code&, boost::asio::deadline_timer *t, int* count){
+
+	if(*count < 5){
+		std::cout << *count << std::endl;
+		++(*count);
+	
+		t->expires_at(t->expires_at() + boost::posix_time::seconds(1));
+		t->async_wait(boost::bind(print,boost::asio::placeholders::error,t,count));
+
+	}
+
+}
+
+int main(){
+
+	int count = 0;
+
+	boost::asio::io_service io;
+	boost::asio::deadline_timer t(io, boost::posix_time::seconds(1));
+	t.async_wait(boost::bind(print,boost::asio::placeholders::error, &t, &count));
+	io.run();
+
+	std::cout << "Final count: " << count << std::endl;
+
+	return 0;
+
+}
